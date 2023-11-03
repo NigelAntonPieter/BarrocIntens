f47b899a24cdb9a4c092b5b07f5a3968355d3393
Brent Albers <93827410+BrentAlbers@users.noreply.github.com>
2023-11-03 12:22:34 +0100

    Alle tabellen aangemaakt
    Alle tabellen aangemaakt.
    User, Note en Company al ingevuld
    Appdbcontext gemaakt
    app.config gemaakt
    user tabel geseed

---
+++ BarrocIntens/Date/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace BarrocIntens.Date
+{
+    internal class AppDbContext : DbContext
+    {
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql(
+                ConfigurationManager.ConnectionStrings["BarrocIntens"].ConnectionString,
+                ServerVersion.Parse("5.7.33-winx64"));
+        }
+
+    }
+}
