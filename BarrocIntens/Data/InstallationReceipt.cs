173684f014ab5ca9e13412e7efe16cd8134490a6
merijn <merijnsweep@gmail.com>
2023-11-13 21:05:56 +0100

    Adjustment Receipt helper code
    Fixes to the code for sending for creating a text file and database of a template receipt

--- BarrocIntens/Data/InstallationReceipt.cs
+++ BarrocIntens/Data/InstallationReceipt.cs
@@ -1,75 +1,38 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace BarrocIntens.Data
-{        public class InstallationReceipt
-        {
-            public int Id { get; set; }
-            public string EmployeeName { get; set; }
-            public string MachineModel { get; set; }
-            public DateTime InstallationDate { get; set; }
-            public decimal ConnectionCosts { get; set; }
-        }
+{
+    public class InstallationReceipt
+    {
+        [Key]
+        public int Id { get; set; }
 
-        public class AppDbContext : DbContext
-        {
-            public DbSet<InstallationReceipt> InstallationReceipts { get; set; }
+        [Required]
+        public string EmployeeName { get; set; }
 
-            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-            {
+        [Required]
+        public string MachineModel { get; set; }
 
-                optionsBuilder.UseSqlServer("Server=localhost;port=3306;user=root;password=KutLuna;database=BarrocIntens");
-            }
-        }
+        [Required]
+        public DateTime InstallationDate { get; set; }
 
-        class Program
-        {
-            static void Main()
-            {
-                Console.WriteLine("Welcome to the Installation Receipt Generator!");
-
-                // Get employee information
-                Console.Write("Enter employee name: ");
-                string employeeName = Console.ReadLine();
-
-                // Get installation details
-                Console.Write("Enter coffee machine model: ");
-                string machineModel = Console.ReadLine();
-
-                Console.Write("Enter installation date (YYYY-MM-DD): ");
-                DateTime installationDate = DateTime.Parse(Console.ReadLine());
-
-                Console.Write("Enter connection costs: ");
-                decimal connectionCosts = decimal.Parse(Console.ReadLine());
-
-                // Save receipt to the database
-                SaveReceiptToDatabase(employeeName, machineModel, installationDate, connectionCosts);
-
-                Console.WriteLine("Installation receipt generated and saved to the database successfully!");
-            }
-
-            static void SaveReceiptToDatabase(string employeeName, string machineModel, DateTime installationDate, decimal connectionCosts)
-            {
-                using (var dbContext = new AppDbContext())
-                {
-                    // Create a new InstallationReceipt object
-                    var receipt = new InstallationReceipt
-                    {
-                        EmployeeName = employeeName,
-                        MachineModel = machineModel,
-                        InstallationDate = installationDate,
-                        ConnectionCosts = connectionCosts
-                    };
-
-                    // Add the receipt to the database
-                    dbContext.InstallationReceipts.Add(receipt);
-                    dbContext.SaveChanges();
-                }
-            }
-        }
+        [Required]
+        public decimal ConnectionCosts { get; set; }
     }
 
+    public class AppDbContext : DbContext
+    {
+        public DbSet<InstallationReceipt> InstallationReceipts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=localhost;port=3306;user=root;password=KutLuna;database=BarrocIntens");
+        }
+    }
+}
--- BarrocIntens/InstallationReceiptWindow.xaml.cs
+++ BarrocIntens/InstallationReceiptWindow.xaml.cs
@@ -1,8 +1,3 @@
-using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
 using System;
@@ -12,61 +7,33 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
-using System;
-using System.IO;
+using BarrocIntens.Date;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
 
-class Program
+namespace BarrocIntens
 {
-    static void Main()
+    public sealed partial class InstallationReceiptWindow : Window
     {
-        Console.WriteLine("Installation receipt assister please enter the relevant information:");
-        Console.Write("Enter employee name: ");
-        string employeeName = Console.ReadLine();
-
-        // Get installation details
-        Console.Write("Enter coffee machine model: ");
-        string machineModel = Console.ReadLine();
-
-        Console.Write("Enter installation date (YYYY-MM-DD): ");
-        string installationDate = Console.ReadLine();
-
-        Console.Write("Enter connection costs: ");
-        decimal connectionCosts = decimal.Parse(Console.ReadLine());
-
-        // Generate receipt
-        string receipt = GenerateReceipt(employeeName, machineModel, installationDate, connectionCosts);
-
-        // Save receipt to a file
-        SaveReceiptToFile(receipt);
-
-        Console.WriteLine("Installation receipt generated and saved successfully!");
-    }
-
-    static string GenerateReceipt(string employeeName, string machineModel, string installationDate, decimal connectionCosts)
-    {
-        string receiptTemplate = $"Installation Receipt\n\n" +
-                                 $"Employee: {employeeName}\n" +
-                                 $"Coffee Machine Model: {machineModel}\n" +
-                                 $"Installation Date: {installationDate}\n" +
-                                 $"Connection Costs: ${connectionCosts}\n\n" +
-                                 "Thank you for choosing our services!";
-
-        return receiptTemplate;
-    }
-
-    static void SaveReceiptToFile(string receipt)
-    {
-        string fileName = "InstallationReceipt.txt";
-
-        try
+        public InstallationReceiptWindow()
         {
-            File.WriteAllText(fileName, receipt);
+            this.InitializeComponent();
         }
-        catch (Exception ex)
+
+        private void GenerateReceiptButton_Click(object sender, RoutedEventArgs e)
         {
-            Console.WriteLine($"Error saving receipt to file: {ex.Message}");
+            string employeeName = EmployeeNameTextBox.Text;
+            string machineModel = MachineModelTextBox.Text;
+            DateTime installationDate = InstallationDatePicker.Date.DateTime;
+            decimal connectionCosts = decimal.Parse(ConnectionCostsTextBox.Text);
+
+            SaveReceiptToDatabase(employeeName, machineModel, installationDate, connectionCosts);
+
+        }
+
+        private void SaveReceiptToDatabase(string employeeName, string machineModel, DateTime installationDate, decimal connectionCosts)
+        {
+
         }
     }
 }
-
-
