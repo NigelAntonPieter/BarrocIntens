58d9308aecdff7efa0f7e49c50aecd2ebbfc8c5e
merijn <merijnsweep@gmail.com>
2023-11-15 21:10:54 +0100

    Edit Delete contract commit
    Adds code to delete or edit existing contracts as well as some of the baseline for the invoices

--- BarrocIntens/Data/AppDbContext.cs
+++ BarrocIntens/Data/AppDbContext.cs
@@ -12,6 +12,8 @@ namespace BarrocIntens.Date
     internal class AppDbContext : DbContext
     {
         public DbSet<User> Users { get; set; }
+        public DbSet<LeaseContract> LeaseContracts { get; set; }
+        public DbSet<InvoiceFinance> Invoices { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
@@ -27,7 +29,18 @@ namespace BarrocIntens.Date
                 new User { Id = 1, Name = "John Doe", UserName = "johndoe", Password = "password123", Role = "Sales" },
                 new User { Id = 2, Name = "Jane Smith", UserName = "janesmith", Password = "password456", Role = "Maintenance"}
             );
+            modelBuilder.Entity<LeaseContract>().HasData(
+          new LeaseContract { Id = 1, CustomerName = "Customer A", BkrCheckPassed = true, MonthlyInvoice = true },
+          new LeaseContract { Id = 2, CustomerName = "Customer B", BkrCheckPassed = false, MonthlyInvoice = false }
+            );
+
+            modelBuilder.Entity<InvoiceFinance>().HasData(
+                new InvoiceFinance { Id = 1, LeaseContractId = 1, DueDate = DateTime.Now.AddDays(30), Amount = 100.00m, IsPaid = false },
+                new InvoiceFinance { Id = 2, LeaseContractId = 2, DueDate = DateTime.Now.AddDays(45), Amount = 150.00m, IsPaid = true }
+            );
+
         }
+
 
     }
 }
--- BarrocIntens/LeaseContractManagementPage.xaml.cs
+++ BarrocIntens/LeaseContractManagementPage.xaml.cs
@@ -14,7 +14,6 @@ using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 
-
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
@@ -42,6 +41,77 @@ namespace BarrocIntens
             dbContext.LeaseContracts.Add(newLeaseContract);
             dbContext.SaveChanges();
         }
-       
+        private void EditLeaseContractButton_Click(object sender, RoutedEventArgs e)
+        {
+            LeaseContract selectedLeaseContract = GetSelectedLeaseContract();
+
+            if (selectedLeaseContract != null)
+            {
+                var editForm = new EditLeaseContractForm(selectedLeaseContract);
+                editForm.ShowDialog();
+            }
+        }
+
+        private void DeleteLeaseContractButton_Click(object sender, RoutedEventArgs e)
+        {
+            LeaseContract selectedLeaseContract = GetSelectedLeaseContract();
+
+            if (selectedLeaseContract != null)
+            {
+                dbContext.LeaseContracts.Remove(selectedLeaseContract);
+                dbContext.SaveChanges();
+            }
+        }
+
+        private void GenerateInvoiceButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Add code to generate an invoice for a lease contract
+
+            LeaseContract selectedLeaseContract = GetSelectedLeaseContract();
+
+            if (selectedLeaseContract != null)
+            {
+                // Create a new invoice based on the selected lease contract
+                InvoiceFinance newInvoice = new InvoiceFinance
+                {
+
+                };
+
+                dbContext.Invoices.Add(newInvoice);
+                dbContext.SaveChanges();
+            }
+        }   
+
+        private void SendInvoiceButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Add code to send an invoice to the customer
+        }
+
+        private void MarkAsPaidButton_Click(object sender, RoutedEventArgs e)
+        {
+            InvoiceFinance selectedInvoice = GetSelectedInvoice();
+
+            if (selectedInvoice != null)
+            {
+                selectedInvoice.IsPaid = true;
+
+                dbContext.SaveChanges();
+
+            }
+        }
+        private LeaseContract GetSelectedLeaseContract()
+        {
+            //if (leaseContractsListView.SelectedItem is LeaseContract selectedLeaseContract)
+            //{
+            //    return selectedLeaseContract;
+            //}
+            return null;
+
+        }
+        private InvoiceFinance GetSelectedInvoice()
+        {
+            // Add code to retrieve the selected invoice from the UI
+            return null;
+        }
     }
 }
