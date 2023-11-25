50baaa56c524f43ef8c10863271f7f88219a66de
Colin-van-Steenhoven <93527036+Colin-van-Steenhoven@users.noreply.github.com>
2023-11-25 02:26:34 +0100

    AppDbContext aangepast
    In de AppDbContext een nieuw bedrijf toegevoegd

--- BarrocIntens/Data/AppDbContext.cs
+++ BarrocIntens/Data/AppDbContext.cs
@@ -13,6 +13,8 @@ namespace BarrocIntens.Date
     {
         public DbSet<User> Users { get; set; }
         public DbSet<Product> Products { get; set; }
+        public DbSet<Note> Notes { get; set; }
+        public DbSet<Company> Companies { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
@@ -29,14 +31,23 @@ namespace BarrocIntens.Date
                .WithMany(c => c.Users)
                .UsingEntity<Note>();
 
-            modelBuilder.Entity<User>().HasData(
+            modelBuilder.Entity<Note>()
+               .HasOne(n => n.Company)
+               .WithMany(c => c.Notes)
+               .HasForeignKey(n => n.CompanyId);
+
+        modelBuilder.Entity<User>().HasData(
                 new User { Id = 1, Name = "John Doe", UserName = "johndoe", Password = "password123", Role = "Sales" },
                 new User { Id = 2, Name = "Jane Smith", UserName = "janesmith", Password = "password456", Role = "Maintenance"},
-                 new User { Id = 3, Name = "Nigel Pieter", UserName = "AnsjoNation", Password = "password4321", Role = "client" }
+                 new User { Id = 3, Name = "Nigel Pieter", UserName = "AnsjoNation", Password = "password4321", Role = "client"}
             );
 
             modelBuilder.Entity<Product>().HasData(
                new Product { Id = 1, Name= "Balonga", Description = "kapot lekker"}
+            );
+
+            modelBuilder.Entity<Company>().HasData(
+               new Company { Id = 1, Name = "Google", Phone = "0612345678", Street = "googlestreet 1" }
             );
         }
 
