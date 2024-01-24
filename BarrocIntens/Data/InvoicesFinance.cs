6aa69d5f2776ad7694e8518203e8e8f68011d7de
NigelAntonPieter <na.pieter@hotmail.nl>
2024-01-24 10:51:02 +0100

    folders
    ik heb de sales en de finances windows in folders gezet zodat het er meer overzichtelijker uit ziet

--- BarrocIntens/Data/InvoicesFinance.cs
+++ BarrocIntens/Data/InvoicesFinance.cs
@@ -1,23 +1,11 @@
 ﻿using System;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 
-namespace BarrocIntens.Data
-{ 
-public class InvoiceFinance
+namespace BarrocIntens.Finance
+{
+    internal class InvoicesFinance
     {
-        [Key]
-        public int Id { get; set; }
-
-        public int LeaseContractId { get; set; }
-        public bool MonthlyInvoice { get; set; }
-        public bool PeriodicInvoice { get; set; }
-        public DateTime DateCreated { get; set; }
+        public string CustomerName { get; set; }
         public decimal Amount { get; set; }
-        public bool IsPaid { get; set; }
-
-        [ForeignKey("LeaseContractId")]
-        public virtual LeaseContract LeaseContract { get; set; }
+        public DateTime DateCreated { get; set; }
     }
-
 }
